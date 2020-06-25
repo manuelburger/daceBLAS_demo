4d88e97a22a97fbb2cd7d79752542b0117e630c2
Manuel Burger <burgerm@student.ethz.ch>
2020-06-25 13:28:46 +0200

    update README

--- setSubmodules.sh
+++ setSubmodules.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Init submodules"
-git submodule update --init
+git submodule update --init --recursive
 
 echo "Set submodules to correct commit"
 cd dace
 git checkout 2a5d30d
-git submodule update --init
+git submodule update --init --recursive
 cd ..
